c342f53a7301e27973912bd4e94600a9827eaab1
abditufa <abdi.tufa24@gmail.com>
2021-06-26 18:13:35 -0500

    Yeah Yeah

--- src/main/java/com/example/WeTutor/config/SecurityConfig.java
+++ src/main/java/com/example/WeTutor/config/SecurityConfig.java
@@ -42,13 +42,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/authenticate").permitAll()
                 .antMatchers("/api/registration").permitAll()
-                .antMatchers("/api/tutor/all").permitAll()
-                .antMatchers("/api/tutor/").permitAll()
-                .antMatchers("/api/tutor").permitAll()/*
-                .antMatchers("/api/profile/").permitAll()
-                .antMatchers("/api/profile/all").permitAll()
-                .antMatchers("/api/profile/create-profile").permitAll()*/
-
                 .anyRequest().authenticated().and()
                 .exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
