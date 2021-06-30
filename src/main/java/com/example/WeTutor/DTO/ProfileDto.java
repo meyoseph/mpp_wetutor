cf04b464b5fc2d737b336a5c521177fda34aa3ae
abdi2fa <abdi.tufa24@gmail.com>
2021-06-29 23:32:28 -0500

    Feature: Create DTO layer for profile.
    GET -- http://localhost:8080/api/profile/
    
    + ProfileDto
     - {
            private String id;
            private String age;
            private String firstName;
            private String lastName;
            private String gender;
            private String majorSubject;
            private String subjects;
            private String educations;
            private String workExperiences;
            private String languages;
            @Enumerated(EnumType.STRING)
            private ProfileState profileState;
            private String tutorId;
        }
    
     + {
           "success": true,
           "profiles": []
       }

---
+++ src/main/java/com/example/WeTutor/DTO/ProfileDto.java
@@ -0,0 +1,29 @@
+package com.example.WeTutor.DTO;
+
+import com.example.WeTutor.entities.ProfileState;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class ProfileDto {
+    private String id;
+    private String age;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String majorSubject;
+    private String subjects;
+    private String educations;
+    private String workExperiences;
+    private String languages;
+    @Enumerated(EnumType.STRING)
+    private ProfileState profileState;
+    private String tutorId;
+}
