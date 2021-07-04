b86ddbce55899c78b5af7d311ff1fdf8036e3af1
meyospeh <meyoseph@gmail.com>
2021-07-04 18:37:29 -0500

    Adds AdminServiceTest specs

--- src/test/java/com/example/WeTutor/services/AdminServiceTest.java
+++ src/test/java/com/example/WeTutor/services/AdminServiceTest.java
@@ -1,10 +1,9 @@
 package com.example.WeTutor.services;
 
-import com.example.WeTutor.entities.Parent;
-import com.example.WeTutor.entities.Role;
-import com.example.WeTutor.entities.User;
+import com.example.WeTutor.entities.*;
 import com.example.WeTutor.repositories.ProfileRepository;
 import com.example.WeTutor.repositories.UserRepository;
+import org.json.simple.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -13,11 +12,13 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 public class AdminServiceTest {
@@ -48,15 +49,87 @@ public class AdminServiceTest {
         System.out.println(user2.getRoles().get(0).getRoleName());
     }
 
-//    public ResponseEntity<Object> getAllParents() {
-//        List<User> users = userRepository.findAll();
-//        List<User> parents = new ArrayList<>();
-//        for(User u: users){
-//            if(u.getRoles().get(0).getRoleName().equals("parent")){
-//                parents.add(u);
-//            }
-//        }
-//
-//        return ResponseEntity.status(HttpStatus.OK).body(parents);
-//    }
+    @Test
+    void itShouldGetProfile(){
+        Role role = new Tutor();
+        User user = new User("test1", "test2", "test@gmail.com", role);
+        Profile profile = new Profile(
+                "27",
+                "yoseph",
+                "birhanu",
+                "male",
+                "+251911072790",
+                "addis ababa",
+                "test",
+                "math",
+                "java, ruby",
+                "degree",
+                "test",
+                "test",
+                "test",
+                user);
+        when(profileRepository.findProfileByTutorId(user.getId())).thenReturn(profile);
+        Profile profile1 = adminService.getProfile(user);
+        assertThat(profile1).isEqualTo(profile1);
+    }
+
+    @Test
+    void itShouldReturnProfileDoesNotExist(){
+        when(profileRepository.findProfileById("123")).thenReturn(null);
+        ResponseEntity<Object> responseEntity = adminService.decideOnProfile("123", false);
+        assertEquals(400, responseEntity.getStatusCodeValue());
+    }
+
+    @Test
+    void itShouldFailWhenUserIdDidntExist(){
+        Role role = new Tutor();
+        User user = new User("test1", "test2", "test@gmail.com", role);
+        Profile profile = new Profile(
+                "27",
+                "yoseph",
+                "birhanu",
+                "male",
+                "+251911072790",
+                "addis ababa",
+                "test",
+                "math",
+                "java, ruby",
+                "degree",
+                "test",
+                "test",
+                "test",
+                user);
+        when(profileRepository.findProfileById(user.getId())).thenReturn(profile);
+        profile.getTutor().setId("");
+        ResponseEntity<Object> responseEntity = adminService.decideOnProfile(profile.getId(), false);
+        assertEquals(400, responseEntity.getStatusCodeValue());
+
+    }
+
+    @Test
+    void itShouldFailWhenUserIsNull(){
+        Role role = new Tutor();
+        User user = new User("test1", "test2", "test@gmail.com", role);
+        user.setId("21");
+        Profile profile = new Profile(
+                "27",
+                "yoseph",
+                "birhanu",
+                "male",
+                "+251911072790",
+                "addis ababa",
+                "test",
+                "math",
+                "java, ruby",
+                "degree",
+                "test",
+                "test",
+                "test",
+                user);
+        when(profileRepository.findProfileById(profile.getId())).thenReturn(profile);
+        when(userRepository.findById(user.getId())).thenReturn(null);
+        ResponseEntity<Object> responseEntity = adminService.decideOnProfile(profile.getId(), false);
+        assertEquals(400, responseEntity.getStatusCodeValue());
+        System.out.println(responseEntity.getBody());
+    }
 }
